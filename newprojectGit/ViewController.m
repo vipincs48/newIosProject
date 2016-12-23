0fa75695493570aada9582cac5f7387079a36aff
Krishna Prasad <krishnaprasad@Krishnas-Mac-mini.local>
2016-12-23 15:16:44 -0800

    dsvfvdfefd

--- newprojectGit/ViewController.m
+++ newprojectGit/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSString *mysr=@"vipin";
+   // NSString *mysr=@"vipin";
 }
 
 
